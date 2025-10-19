84e60bb31235d0c8354979bd4b6c9584df5d9e38
0xA5H15H <aashishdhanda007@gmail.com>
2025-10-18 21:12:21 -0400

    Updated Supabase URL and Public Key

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // Environment configuration
 // This file will be automatically populated by Vercel with environment variables
 window.ENV = {
-    SUPABASE_URL: '__SUPABASE_URL__',
-    SUPABASE_ANON_KEY: '__SUPABASE_ANON_KEY__'
+    SUPABASE_URL: 'https://btzwlhqecgodnsomporz.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0endsaHFlY2dvZG5zb21wb3J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MDk3OTIsImV4cCI6MjA3NjM4NTc5Mn0.x-HIDwH10F4gGnqy5bDPEpuwBM1R0Qg-8u72aVtpe8Q'
 };
